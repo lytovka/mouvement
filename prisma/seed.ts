4b27b72fc650e9a786c6a20cee4119049c210521
ivanlytovka <ivanlytovka@gmail.com>
2024-02-03 12:46:40 -0500

    seed mock movements for each style

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
 import fs from "fs/promises"
+import { faker } from '@faker-js/faker'
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
@@ -12,14 +13,7 @@ const img = async ({altText, filepath}: {altText: string, filepath: string}) => 
 	}
 }
 
-async function seed(){
-	console.log('🌱 Seeding...')
-	console.time(`🌱 Database has been seeded`)
-
-	console.time('🧹 Cleaned up the database...')	
-	await prisma.style.deleteMany()
-	console.timeEnd('🧹 Cleaned up the database...')
-
+async function seedStyles(){
 	const totalStyles = 3
 	console.time(`🕺 Created ${totalStyles} styles...`)
 
@@ -30,17 +24,51 @@ async function seed(){
 	img({altText: 'afrodance', filepath: './tests/fixtures/images/styles/afrodance.png'}),
 	])
 
+	const movementImages = await Promise.all([
+	img({
+			altText: "Dance 1",
+			filepath: "./tests/fixtures/images/movements/dance1.webp"})
+	])
+
+	const stylesIds: Array<number> = []
 	for(let i = 0; i < totalStyles; i++){
-		await prisma.style.create({
+		const {id} = await prisma.style.create({
 			select: {id: true},
 			data: {
 			name: styles[i % totalStyles], 
-				img: {create: stylesImages[i % totalStyles]}
+				img: {create: stylesImages[i % totalStyles]},
+				movements: {
+				create: Array.from({length: faker.number.int({min: 2, max: 5})}).map(() => ({
+						name: faker.lorem.words({min: 1, max: 6}),
+					content: faker.lorem.paragraphs({min: 1, max: 3}),
+						images: {
+							// TODO: use random length for movement varations
+							create: Array.from({length: 1}).map(() => {
+								return movementImages[0]
+							})
+						},
+					}))
+				}
 			}
 		})
+
+		stylesIds.push(id)
 	}
 	console.timeEnd(`🕺 Created ${totalStyles} styles...`)
 	console.timeEnd(`🌱 Database has been seeded`)
+}
+
+async function seedMovements(styleIds: Array<number>) {
+}
+
+async function seed(){
+	console.log('🌱 Seeding...')
+	console.time(`🌱 Database has been seeded`)
+
+	console.time('🧹 Cleaned up the database...')	
+	await prisma.style.deleteMany()
+	console.timeEnd('🧹 Cleaned up the database...')
+	await seedStyles()
 }
 
 seed()
